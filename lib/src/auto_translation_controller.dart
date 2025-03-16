141572d6c4bca6985f6bf50c2448be0bc9850479
robmllze <86869208+robmllze@users.noreply.github.com>
2025-03-16 19:18:53 +1100

    fix: Bugfix

--- lib/src/auto_translation_controller.dart
+++ lib/src/auto_translation_controller.dart
@@ -20,10 +20,9 @@ import '/_common.dart';
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
 class AutoTranslationController<
-  TRemoteDatabaseInterface extends DatabaseInterface,
-  TCachedDatabaseInterface extends DatabaseInterface,
-  TTranslationInterface extends TranslatorInterface
-> {
+    TRemoteDatabaseInterface extends DatabaseInterface,
+    TCachedDatabaseInterface extends DatabaseInterface,
+    TTranslationInterface extends TranslatorInterface> {
   //
   //
   //
@@ -78,10 +77,8 @@ class AutoTranslationController<
     await _initLocalePod();
     if (locale != null) {
       await _pLocale!.set(locale);
-    }
-    assert(locale != null && this.locale != null, 'Locale is null!');
-    if (this.locale == null) {
-      return;
+    } else if (this.locale == null) {
+      await _pLocale!.set(primaryLocale(WidgetsBinding.instance));
     }
     final a = await _loadTranslations(remoteDatabaseBroker, this.locale!);
     final b = _loadTranslations(persistentDatabaseBroker, this.locale!).then((
@@ -203,14 +200,13 @@ class AutoTranslationController<
     //   '[TranslationController._createTranslationManager] Did not get translation for key: $key. Attempting to translate...',
     // );
 
-    final translated =
-        await translationBroker
-            .translateSentence(
-              text: defaultValue,
-              languageCode: this.locale!.languageCode,
-              countryCode: this.locale!.countryCode,
-            )
-            .value;
+    final translated = await translationBroker
+        .translateSentence(
+          text: defaultValue,
+          languageCode: this.locale!.languageCode,
+          countryCode: this.locale!.countryCode,
+        )
+        .value;
 
     // If the translation fails, no more attemps will be made since the
     // key is already added to _didRequestTranslate. This is deliberate to
@@ -219,45 +215,36 @@ class AutoTranslationController<
 
     // Update the cache in memory with the translated text.
     _pCache.update(
-      (e) =>
-          e
-            ..[key] = TranslatedText(
-              to: translated.unwrap(),
-              from: defaultValue,
-            ),
+      (e) => e
+        ..[key] = TranslatedText(
+          to: translated.unwrap(),
+          from: defaultValue,
+        ),
     );
 
     final path = _databasePath(translationPath, this.locale!);
 
     // Update the persistent database.
-    final futureResult1 =
-        persistentDatabaseBroker
-            .patch(
-              path: path,
-              data: {
-                key:
-                    TranslatedText(
-                      to: translated.unwrap(),
-                      from: defaultValue,
-                    ).toMap(),
-              },
-            )
-            .value;
+    final futureResult1 = persistentDatabaseBroker.patch(
+      path: path,
+      data: {
+        key: TranslatedText(
+          to: translated.unwrap(),
+          from: defaultValue,
+        ).toMap(),
+      },
+    ).value;
 
     // Update the remote database.ßå
-    final futureResult2 =
-        remoteDatabaseBroker
-            .patch(
-              path: path,
-              data: {
-                key:
-                    TranslatedText(
-                      to: translated.unwrap(),
-                      from: defaultValue,
-                    ).toMap(),
-              },
-            )
-            .value;
+    final futureResult2 = remoteDatabaseBroker.patch(
+      path: path,
+      data: {
+        key: TranslatedText(
+          to: translated.unwrap(),
+          from: defaultValue,
+        ).toMap(),
+      },
+    ).value;
 
     final results = await Future.wait([futureResult1, futureResult2]);
 

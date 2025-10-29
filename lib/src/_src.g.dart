1f33e47c7e600941245171de6d3327bc6bd4dfec
t0mb3rr <robmllze@gmail.com>
2025-10-29 17:11:46 +1100

    +Small bugfix

--- lib/src/_src.g.dart
+++ lib/src/_src.g.dart
@@ -11,8 +11,6 @@ export './auto_translation_controller.dart';
 export './auto_translation_scope.dart';
 export './implementations/claude_translator_broker.dart';
 export './implementations/firebase_auth_broker.dart';
-export './implementations/firestore_database_broker/convert_to_firestore.dart';
-export './implementations/firestore_database_broker/convert_to_local.dart';
 export './implementations/firestore_database_broker/firestore_database_broker.dart';
 export './implementations/gemeni_translator_broker.dart';
 export './implementations/google_translator_broker.dart';
